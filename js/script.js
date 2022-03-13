3b8dd9d7b6152742411f31b8da4a6ca33129200d
ZuzanaGallova <zuzka.gallova02@gmail.com>
2022-03-13 21:21:53 +0100

    It's getting there

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
-const dots = document.getElementById("dots");
+const dots = document.querySelector(".fa-ellipsis-vertical");
 const addMenu = () => {
     const nav = document.querySelector('nav')
     nav.classList.toggle("menu");
 }
-dots.addEventListener("click", addMenu)
+dots.addEventListener("click", addMenu);
+
+let popup = document.getElementById("about"); //find the button
+let closeIt = document.querySelector("main"); //find the x
+const photo = document.querySelector('.photo'); //find the video
+
+popup.addEventListener("click", function(){ //does anzbodz press it
+    photo.classList.toggle('toggleShow'); //add the class "toggleshow" to the video element
+})
+closeIt.addEventListener("click", function(){//does anzbodz press the x button
+    photo.classList.remove('toggleShow');//remove the class
+})
