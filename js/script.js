bac81fd44011829d0235637c1d6462521c310a20
ZuzanaGallova <zuzka.gallova02@gmail.com>
2022-03-24 13:40:26 +0100

    95% doneeeeee

--- js/script.js
+++ js/script.js
@@ -5,13 +5,39 @@ const addMenu = () => {
 }
 dots.addEventListener("click", addMenu);
 
-let popup = document.getElementById("about");
-let closeIt = document.querySelector("main"); 
+/*let popup = document.getElementById("about");
+let closeIt = document.getElementById("x"); 
 const photo = document.querySelector('.photo'); 
+let clicked = false*/
 
-popup.addEventListener("click", function(){ 
+/*popup.addEventListener("click", function(){ 
+    console.log(this)
     photo.classList.toggle('toggleShow'); 
-})
+    if (this.id){
+        closeIt.addEventListener("click", function(){
+            console.log("his")
+            photo.classList.remove('toggleShow');
+        })
+    }
+})*/
+/*const pop = () =>{
+    if(clicked){
+        photo.classList.toggle('toggleShow');
+}
+    else{
+        photo.classList.remove('toggleShow');
+    }
+}
+const changeBoolean = () => { 
+    if (clicked){
+        clicked = false;
+    }else {
+        clicked = true;
+    }
+    pop()
+    } 
+
+popup.addEventListener("click", changeBoolean);
 closeIt.addEventListener("click", function(){
     photo.classList.remove('toggleShow');
-})
+})*/
